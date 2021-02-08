c8070e9ffec731f256647e30a6f65ce686444784
d8ahazard <d8ahazard@gmail.com>
2021-02-08 08:36:39 -0600

    Fix script path

--- stage2/05-glimmr/02-run.sh
+++ stage2/05-glimmr/02-run.sh
@@ -10,7 +10,7 @@ rm -rf ${ROOTFS_DIR}/home/glimmrtv/glimmr
 git clone -b dev https://github.com/d8ahazard/glimmr ${ROOTFS_DIR}/home/glimmrtv/glimmr
 # Install update script to init.d 
 sudo chmod 777 ${ROOTFS_DIR}/home/glimmrtv/glimmr/update_pi.sh
-sudo ln -sf ${ROOTFS_DIR}/home/glimmrtv/glimmr/update_pi.sh ${ROOTFS_DIR}/etc/init.d/update_glimmr.sh
+sudo ln -sf ${ROOTFS_DIR}/home/glimmrtv/glimmr/script/update_pi.sh ${ROOTFS_DIR}/etc/init.d/update_glimmr.sh
 cd ${ROOTFS_DIR}/home/glimmrtv/glimmr
 dotnet publish ./src/Glimmr.csproj /p:PublishProfile=LinuxARM -o ${ROOTFS_DIR}/bin/
 cp -r ${ROOTFS_DIR}/home/glimmrtv/glimmr/lib/bass.dll ${ROOTFS_DIR}/usr/lib/bass.dll
