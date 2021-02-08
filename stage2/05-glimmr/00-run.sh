eddac7157448f70b8e9d8001c41fc52b3f635e27
d8ahazard <d8ahazard@gmail.com>
2021-02-08 08:13:08 -0600

    Fix usermod root

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
-useradd -R -m glimmrtv
-usermod -R -aG sudo glimmrtv 
+usermod -R ${ROOTFS_DIR} -aG sudo glimmrtv 
 mkdir -p ${ROOTFS_DIR}/home/glimmrtv
 cd ${ROOTFS_DIR}/home/glimmrtv
 # Check dotnet installation
