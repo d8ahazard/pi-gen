0813e4bf1ba796c49493d208e2a9b089c3dde89a
d8ahazard <d8ahazard@gmail.com>
2021-02-08 09:22:49 -0600

    Update 00-run.sh

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -6,6 +6,7 @@ cd ${ROOTFS_DIR}/home/glimmrtv
 rm -rf ${ROOTFS_DIR}/home/glimmrtv/dotnet-sdk*
 wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-sdk-latest-linux-arm.tar.gz
 sudo mkdir -p ${ROOTFS_DIR}/usr/share/dotnet
+sudo mkdir -p /usr/share/dotnet
 sudo tar -zxf dotnet-sdk-latest-linux-arm.tar.gz -C ${ROOTFS_DIR}/usr/share/dotnet
 sudo tar -zxf dotnet-sdk-latest-linux-arm.tar.gz -C /usr/share/dotnet
 sudo ln -sf ${ROOTFS_DIR}/usr/share/dotnet/dotnet /bin/dotnet
