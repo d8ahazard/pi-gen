5d12d0f9acf4ace50d54e734548e7898dbdd34ef
d8ahazard <d8ahazard@gmail.com>
2021-02-08 08:33:45 -0600

    Update 00-run.sh

--- stage2/05-glimmr/00-run.sh
+++ stage2/05-glimmr/00-run.sh
@@ -3,7 +3,7 @@ install -m 644 files/comitup.conf ${ROOTFS_DIR}/etc/comitup.conf
 mkdir -p ${ROOTFS_DIR}/home/glimmrtv
 cd ${ROOTFS_DIR}/home/glimmrtv
 # Check dotnet installation
-rm rf ${ROOTFS_DIR}/home/glimmrtv/dotnet-sdk*
+rm -rf ${ROOTFS_DIR}/home/glimmrtv/dotnet-sdk*
 wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-sdk-latest-linux-arm.tar.gz
 sudo mkdir -p ${ROOTFS_DIR}/usr/share/dotnet
 sudo tar -zxf dotnet-sdk-latest-linux-arm.tar.gz -C ${ROOTFS_DIR}/usr/share/dotnet
